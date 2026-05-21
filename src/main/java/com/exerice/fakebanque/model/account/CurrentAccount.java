58131ec1b69cd67eac5ef3dcf06396786def10c4
jorism120 <jorism120@gmail.com>
2026-05-21 15:32:06 +0200

    Delete src directory
    delete src

--- src/main/java/com/exerice/fakebanque/model/account/CurrentAccount.java
+++
@@ -1,7 +0,0 @@
-package com.exerice.fakebanque.model.account;
-
-public class CurrentAccount extends WithdrawalAccount {
-    public CurrentAccount(String iban, double initialBalance) {
-        super(iban, initialBalance);
-    }
-}
--- src/main/java/com/exerice/fakebanque/model/account/SavingsAccount.java
+++
@@ -1,7 +0,0 @@
-package com.exerice.fakebanque.model.account;
-
-public class SavingsAccount extends WithdrawalAccount {
-    public SavingsAccount(String iban, double initialBalance) {
-        super(iban, initialBalance);
-    }
-}
