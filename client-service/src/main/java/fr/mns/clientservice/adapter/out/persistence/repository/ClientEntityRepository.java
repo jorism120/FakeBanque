7b39bde4eaf0e54bb52276e2f9a1c7b4091e8d57
jmichel <joris.michel@stagiairesmns.fr>
2026-07-07 16:20:41 +0200

    Fix base de données des clients

--- client-service/src/main/java/fr/mns/clientservice/adapter/out/persistence/repository/ClientEntityRepository.java
+++ client-service/src/main/java/fr/mns/clientservice/adapter/out/persistence/repository/ClientEntityRepository.java
@@ -2,6 +2,8 @@ package fr.mns.clientservice.adapter.out.persistence.repository;
 
 import fr.mns.clientservice.adapter.out.persistence.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientEntityRepository extends JpaRepository<ClientEntity, String> {
 }
---
+++ client-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE client;
+
+INSERT INTO client (id, email, first_name, last_name, phone)
+VALUES ('54545', 'mail@mail.fr', 'Jean', 'Bon', '+2545557');
+
